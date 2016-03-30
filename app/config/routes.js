40751ab2550d6ab751928739b06138a1391ee0b1
Daniel W. Robert <danielwrobert@gmail.com>
2016-03-30 17:13:17 -0400

    Updated File component to GitHubFile. Moved state to Nav from Main.

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Main from '../components/Main';
-import FileContent from '../components/FileContent';
+import GitHubFile from '../components/GitHubFile';
 import Home from '../components/Home';
 import { Route, IndexRoute } from 'react-router';
 
 export default (
 	<Route path="/" component={ Main }>
-		<Route path="component/:filename" component={ File } />
+		<Route path="component/:filename" component={ GitHubFile } />
 		<IndexRoute component={ Home } />
 	</Route>
 );
