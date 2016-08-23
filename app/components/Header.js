5a8e9f96fff250ce4cefd8d3652970d8071945de
Daniel W. Robert <danielwrobert@gmail.com>
2016-08-23 09:44:41 -0400

    Added Link from React Router to header home-page link.

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Header = () => {
 	return (
 		<nav className="navbar navbar-default">
 			<div className="navbar-header" style={{ paddingBottom: 20 }}>
 				<h1>
-					<a className="navbar-brand" href="/public">Theme Pattern File Browser</a>
+					<Link className="navbar-brand" to="/">Theme Pattern File Browser</Link>
 				</h1>
 			</div>
 		</nav>
